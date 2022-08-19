da91493abc59366153074e3034d1beafdac851d1
Siddhi-06 <106826002+Siddhi-06@users.noreply.github.com>
2022-08-19 06:04:35 -0700

    Add files via upload

---
+++ program2.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+
+class program2
+{
+    public static void main(String b[])
+    {
+        System.out.println("Jay Ganesh");
+    }
+}
---
+++ program7.java
@@ -0,0 +1,41 @@
+// Template for probplems on Pattern printing
+
+import java.lang.*;
+import java.util.*;
+
+class Pattern
+{
+    private int iRow, iCol;
+
+    public Pattern(int a, int b)
+    {
+        iRow = a;
+        iCol = b;
+    }
+
+    public void DisplayPattern()
+    {
+        int i =0,j=0;
+        for(i=0;i<=iRow;i++)
+        {
+            for(j=0;j<=iCol;j++)
+            {
+                System.out.print("*\t");
+            }
+            System.out.println();
+         }
+
+        }
+     }
+    
+
+
+class program7
+{
+    public static void main(String arg[])
+    {
+            Pattern obj = new Pattern(4,4);
+
+            obj.DisplayPattern();
+    }
+}
