a7af68d9c8eedce58f580be65244bb34232b5eb7
Siddhi-06 <106826002+Siddhi-06@users.noreply.github.com>
2022-08-19 06:06:39 -0700

    Add files via upload

---
+++ program20.java
@@ -0,0 +1,33 @@
+
+import java.lang.*;
+import java.util.*;
+
+
+class Bitwise
+{
+    public int OnBit(int iNo)       
+    {
+        int iMask = 0X00000048;
+        int iResult = 0;
+
+        iResult = iNo ^ iMask;
+        return iResult;
+    }
+}
+
+class program20
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);  
+
+        System.out.println("Enter number ");
+        int value = sobj.nextInt();
+
+        Bitwise bobj = new Bitwise();
+        int iret = bobj.OnBit(value);
+        System.out.println("Updated number is  : "+iret);
+    }
+}
+
+
---
+++ program21.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+import java.util.*;
+
+
+class Bitwise
+{
+
+}
+
+class program21
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);  
+
+        System.out.println("Enter number ");
+        int value = sobj.nextInt();
+
+        System.out.println("Enter position ");
+        int pos = sobj.nextInt();
+
+        Bitwise bobj = new Bitwise();
+
+    }
+}
+
